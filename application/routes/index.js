8e8383754b9127d4b047d38a44aeb67ce2fe7fa3
Burns <65887526+Dylan-Burns@users.noreply.github.com>
2023-02-26 11:12:48 -0800

    Add files via upload

--- application/routes/index.js
+++ application/routes/index.js
@@ -10,4 +10,28 @@ router.get('/learnMore', function(req, res) {
   res.render('learnMore', { title: 'Personal Profile' });
 });
 
+router.get('/learnMoreDylan', function(req, res) {
+  res.render('learnMoreDylan', { title: 'Personal Profile' });
+});
+
+router.get('/learnMoreTim', function(req, res) {
+  res.render('learnMoreTim', { title: 'Personal Profile' });
+});
+
+router.get('/learnMoreJeremiah', function(req, res) {
+  res.render('learnMoreJeremiah', { title: 'Personal Profile' });
+});
+
+router.get('/learnMoreKevin', function(req, res) {
+  res.render('learnMoreKevin', { title: 'Personal Profile' });
+});
+
+router.get('/learnMoreWallace', function(req, res) {
+  res.render('learnMoreWallace', { title: 'Personal Profile' });
+});
+
+router.get('/learnMoreChristian', function(req, res) {
+  res.render('learnMoreChristian', { title: 'Personal Profile' });
+});
+
 module.exports = router;
