fc22dde08cd4fb13d4a6abc47c485bc5806ebbda
Kevin Aziz <kaziz2@mail.sfsu.edu>
2023-04-07 22:31:22 -0700

    Most File Changed

---
+++ application/config/db.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "34.94.177.91",
+  user: "root",
+  password: "*4M3K0pOd(Y?dP?t"
+ 
+
+});
+
+con.connect(function(err) {
+ 
+  if (err) throw err;
+  console.log("Connected!");
+});
+module.exports = {con};
