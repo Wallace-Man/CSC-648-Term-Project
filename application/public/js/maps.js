3ea6e3d17c0f083a73b844e459c5c6d6b4b110a0
Dylan Burns <65887526+Dylan-Burns@users.noreply.github.com>
2023-04-14 02:05:07 -0700

    added geolocation to suggest nearby restaurants

--- application/public/js/maps.js
+++ application/public/js/maps.js
@@ -1,66 +1,51 @@
-// import {google} from "googleapis";
-
 function initMap() {
-    // Create a map object centered on your location
-    var map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 37.720664, lng: -122.474497 },
-        zoom: 14,
-    });
-
-    // Create a search box and link it to the UI element
-    var input = document.getElementById("search-bar");
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the search box results towards the current map's viewport
-    map.addListener("bounds_changed", function () {
-        searchBox.setBounds(map.getBounds());
-    });
-
-    // Listen for the event triggered when the user selects a prediction and retrieve
-    // more details for that place
-    searchBox.addListener("places_changed", function () {
-        var places = searchBox.getPlaces();
-
-        if (places.length === 0) {
-            return;
-        }
-
-        // Clear out the old markers
-        markers.forEach(function (marker) {
-            marker.setMap(null);
-        });
-        let markers = [];
-
-        // For each place, get the icon, name and location
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function (place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25),
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             };
-
-            // Create a marker for each place
-            var marker = new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location,
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: userLocation
             });
-
-            markers.push(marker);
-
-            bounds.extend(place.geometry.location);
+            var service = new google.maps.places.PlacesService(map);
+            var request = {
+                location: userLocation,
+                radius: '1500',
+                type: ['restaurant']
+            };
+            service.nearbySearch(request, function(results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        createMarker(results[i], map);
+                    }
+                }
+            });
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
         });
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
 
-        // Center the map on the markers
-        map.fitBounds(bounds);
+function createMarker(place, map) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
     });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
