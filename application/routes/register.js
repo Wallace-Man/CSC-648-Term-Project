56a917e1722afb002025c87e0ef629cf144ed78d
Jeremiah Ruvalcaba <jeremiah.ruvalcaba@Jeremiahs-MacBook-Pro-2.local>
2023-04-29 10:17:59 -0700

    added register.js and it's route in app.js

---
+++ application/routes/register.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const util = require('util');
+
+// Create a MySQL connection
+const connection = mysql.createConnection({
+    host: "34.94.177.91",
+    user: "jruv",
+    password: "12345",
+    database: "restaurantdb",
+  });
+  
+  // Connect to the MySQL database
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the database.');
+  });
+
+
+  //rounds for bcrypt
+const saltRounds = 10;
+
+// POST request for sign up
+router.post('/authorization', async (req, res) => {
+    const { username, email, password, password2 } = req.body;
+  
+    if (password != password2) {
+      res.render('register', { error: 'Passwords do not match. Please try again.' });
+      return;
+    }
+  
+    try {
+      // Promisify the connection.query function
+      const queryPromise = util.promisify(connection.query).bind(connection);
+  
+      // Hash the password before storing it in the database
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+  
+      // Replace the original password with the hashed password in the query
+      const query = 'INSERT INTO user (username, email, password) VALUES (?, ?, ?)';
+      const results = await queryPromise(query, [username, email, hashedPassword]);
+  
+      if (results.affectedRows > 0) {
+        res.redirect('/');
+        console.log('Account Created!');
+      } else {
+        res.render('register', { error: 'Unable to create an account. Please try again.' });
+      }
+    } catch (err) {
+      console.error('Error during registration:', err);
+      res.status(500).send('Internal Server Error: ' + err.message);
+    }
+  });
+  
+  module.exports = router;
--- application/routes/users.js
+++ application/routes/users.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
+const util = require('util');
+const bcrypt = require('bcrypt');
 
 
 // Create a MySQL connection
-const dbConnection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "34.94.177.91",
     user: "jruv",
     password: "12345",
@@ -13,7 +15,7 @@ const dbConnection = mysql.createConnection({
   });
   
   // Connect to the MySQL database
-  dbConnection.connect((err) => {
+  connection.connect((err) => {
     if (err) {
       console.error('Error connecting to the database:', err);
       return;
@@ -21,18 +23,37 @@ const dbConnection = mysql.createConnection({
     console.log('Connected to the database.');
   });
 
-
- /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+  router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const query = 'SELECT * FROM user WHERE username = ?';
+  
+    try {
+      // Promisify the connection.query function
+      const queryPromise = util.promisify(connection.query).bind(connection);
+  
+      // Query the database for a user with the given username
+      const results = await queryPromise(query, [username]);
+  
+      if (results.length > 0) {
+        const user = results[0];
+        
+        // Compare the hashed password in the database with the provided password
+        const passwordMatches = await bcrypt.compare(password, user.password);
+  
+        if (passwordMatches) {
+          res.redirect('/');
+        } else {
+          console.log(res.render('login', { error: 'Invalid username or password' }));
+          
+        }
+      } else {
+        console.log(res.render('login', { error: 'Invalid username or password' }));
+      }
+    } catch (err) {
+      console.error('Error during login:', err);
+      res.status(500).send('Internal Server Error: ' + err.message);
+    }
   });
-
-
-  router.post('/signup', function(req, res, next) {
-    console.log(req)
-    res.send('data');
-  });
-
-
-// Export the router to be used in the main application
-module.exports = router;
+  
+  module.exports = router;
+  
