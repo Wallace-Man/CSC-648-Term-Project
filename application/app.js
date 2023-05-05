b5586449eda7d6f560b69219b6f8e9008cf52c0a
Kevin Aziz <kaziz2@mail.sfsu.edu>
2023-05-04 19:58:56 -0700

    Middleware for user session added.

--- application/app.js
+++ application/app.js
@@ -3,13 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const restaurantRouter = require('./routes/restaurants');
-const usersRouter = require('./routes/users');
+const { router: usersRouter, ensureAuthenticated } = require('./routes/users');
 const registerRouter = require('./routes/register');
 const driverRouter = require('./routes/driver');
-
 
 const app = express();
 
@@ -21,29 +21,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Change to 'true' for HTTPS
+}));
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   next();
 });
 
+// Middleware to make user data available to Pug templates
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/',indexRouter);
 app.use('/restaurants', restaurantRouter);
 app.use('/', usersRouter);
 app.use('/', registerRouter);
 app.use('/', driverRouter);
 
-//app.use('/images', express.static('public/images'));
-
-// app.use((req, res) => res.status(404).send('Not Found'));
-
-
 const port = parseInt(process.env.PORT) || 8081;
 
-// if process is a test environment, don't have the app listen on the port
 if (process.env.NODE_ENV !== 'test') {
-  // otherwise listen on port
   app.listen(port, () => {
     console.log(`application: listening on port ${port}`);
   });
