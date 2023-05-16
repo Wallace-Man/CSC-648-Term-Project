330bc1c8a0116a0231c6d4884927d9d7d03f66a0
Jeremiah Ruvalcaba <jruvalcaba@mail.sfsu.edu>
2023-05-15 23:07:55 -0700

    pull test

--- application/public/js/restaurantMenu.js
+++ application/public/js/restaurantMenu.js
@@ -2,6 +2,82 @@
     This file contains the JavaScript code to render and interact with the restaurant menu page. 
     It is implemented using local storage to store the cart items.
 */
+
+//Define the restaurant data as separate arrays (temporary)
+const restaurantInfo = {
+    name: "Roma Antica",
+    rating: 4.5,
+    category: "Italian",
+    address: "123 Main St, Anytown USA",
+    website: "www.restaurantname.com",
+    phone: "(123) 456-7890",
+};
+  
+const menuItems = [
+    {
+      name: "Spaghetti Bolognese",
+      description: "Delicious spaghetti with a classic Bolognese sauce.",
+      price: 12
+    },
+    {
+      name: "Margherita Pizza",
+      description: "Fresh mozzarella, basil, and tomato sauce on a thin crust.",
+      price: 10
+    },
+    {
+      name: "Chicken Alfredo",
+      description: "Creamy fettuccine pasta with grilled chicken and parmesan cheese.",
+      price: 14
+    },
+    {
+      name: "Veggie Burger",
+      description: "A delicious vegetarian burger made with a soy patty, avocado, and chipotle mayo.",
+      price: 9
+    },
+    {
+      name: "Fish and Chips",
+      description: "Golden-brown battered fish served with crispy French fries and tartar sauce.",
+      price: 13
+    }
+];
+
+
+// Generate the HTML code dynamically for restaurant info
+const restaurantInfoHTML = `
+    <div id="restaurant-info">
+        <h3>${restaurantInfo.name}</h3>
+        <p>Rating: ${restaurantInfo.rating} stars</p>
+        <p>Category: ${restaurantInfo.category}</p>
+        <p>Address: ${restaurantInfo.address}</p>
+        <p>Website: ${restaurantInfo.website}</p>
+        <p>Phone: ${restaurantInfo.phone}</p>
+    </div>
+    <img src="https://via.placeholder.com/150">
+`;
+  
+// Generate the HTML code dynamically for menu items
+const menuItemsHTML = menuItems.map(item => `
+    <div class="restaurant-card">
+        <h3>${item.name}</h3>
+        <p>${item.description}</p>
+        <p class="price">$${item.price}</p>
+        <button>Add to cart</button>
+    </div>
+`).join("");
+
+// Insert the dynamically generated HTML code into the DOM
+const restaurantMain = document.getElementById("restaurant-main");
+restaurantMain.innerHTML = `
+    <div id="restaurant-header">
+        ${restaurantInfoHTML}
+    </div>
+    <div id="restaurant-menu-container">
+        <div id="restaurant-menu">
+            ${menuItemsHTML}
+        </div>
+    </div>
+`;  
+  
 import { addToCart, calculateTotal } from './cart.js';
 // get the "Add to Cart" buttons
 const addToCartButtons = document.querySelectorAll('.restaurant-card button');
@@ -71,4 +147,11 @@ function updateCart() {
 
 document.addEventListener('DOMContentLoaded', () => {
     updateCart();
-  });
+    // fetch('/returnMenu')
+    //     .then(function(response) {
+    //         return response.json();
+    //     })
+    //     .then(function(allRestaurants) {
+    //         displayRestaurants(allRestaurants);
+    // });
+});
