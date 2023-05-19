afcb2affed4ed8d1daf26ddbd86caf44174df831
Timothy Polich <tpolich@sfsu.edu>
2023-05-19 11:38:34 -0700

    Added form validation to signup.pug.

---
+++ application/public/js/password.js
@@ -0,0 +1,11 @@
+function validatePassword() {
+    let password = document.getElementById("password").value;
+    let password2 = document.getElementById("password2").value;
+
+    if(password !== password2) {
+        alert("Passwords do not match.");
+        return false;
+    }
+    
+    return true;
+}
